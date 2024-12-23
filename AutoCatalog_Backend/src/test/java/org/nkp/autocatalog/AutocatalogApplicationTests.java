d8a34aaa46f8ad87b68b67c89efcd36db9490fa4
Kras13 <paskalev.1989@abv.bg>
2024-12-23 21:24:21 +0200

    Initial commit.

---
+++ AutoCatalog_Backend/src/test/java/org/nkp/autocatalog/AutocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nkp.autocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
