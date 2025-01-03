8500e76199b753e88a3d0cbc1124eca5cb1972dc
Kras13 <paskalev.1989@abv.bg>
2025-01-03 21:17:18 +0200

    Login and registration.

--- AutoCatalog_Frontend/src/App.tsx
+++ AutoCatalog_Frontend/src/App.tsx
@@ -1,39 +1,89 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import CarList from "./components/CarList";
 import CarDetails from "./components/CarDetails";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
-  const cars = [
-    {
-      id: 1,
-      title: "Audi A4 2.0 TDI",
-      price: 15000,
-      yearManufactured: 2002,
-      fuel: "Diesel",
-      kilometers: 200000,
-    },
-    {
-      id: 2,
-      title: "Toyota Rav-4",
-      price: 7500,
-      yearManufactured: 1998,
-      fuel: "Petrol",
-      kilometers: 300000,
-    },
-    {
-      id: 3,
-      title: "Honda Accord 2.4I",
-      price: 12000,
-      yearManufactured: 2007,
-      fuel: "Petrol",
-      kilometers: 200000,
-    },
-  ];
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+
+    console.log(token);
+
+    if (token) {
+      try {
+        const decoded: any = jwtDecode(token);
+
+        console.log(decoded);
+
+        setUsername(decoded.sub || null);
+      } catch (error) {
+        console.error("Failed to decode JWT token:", error);
+        setUsername(null);
+      }
+    } else {
+      setUsername(null);
+    }
+  }, []);
 
   return (
     <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Car catalog
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {localStorage.getItem("jwtToken") == null ? (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              ) : null}
+              {localStorage.getItem("jwtToken") == null && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              )}
+            </ul>
+            <ul className="nav justify-content-end">
+              {username && (
+                <li
+                  className="nav-item"
+                  onClick={() => {
+                    localStorage.removeItem("jwtToken");
+                    setUsername(null);
+                  }}
+                >
+                  <Link className="nav-link" to="#">
+                    Log out
+                  </Link>
+                </li>
+              )}
+            </ul>
+            <ul className="nav justify-content-end">
+              {username && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    {username}
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
       <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<CarList />} />
         <Route path="/car/:carId" element={<CarDetails />} />
       </Routes>
